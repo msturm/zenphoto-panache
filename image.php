2b6a5ba0703fbce2daa7014e68b3ae6382f15b2a
Martin Sturm <msturm@ebay.com>
2012-11-21 23:29:58 +0100

    Fix image design

--- image.php
+++ image.php
@@ -1,8 +1,3 @@
-<?php												
- 
-	include_once(dirname(__FILE__) . '/../../../includes/language.php'); 
-	include_once(dirname(__FILE__) . '/../../../config.php');
-?>
 <?php if (!defined('WEBPATH')) die(); $themeResult = getTheme($zenCSS, $themeColor, 'light'); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/2002/REC-xhtml1-20020801/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -11,7 +6,7 @@
 	<title><?php echo getBareGalleryTitle(); ?> | <?php echo getBareAlbumTitle();?> | <?php echo getBareImageTitle();?></title>
 	<meta http-equiv="content-type" content="text/html; charset=<?php echo getOption('charset'); ?>" />
 	<link rel="stylesheet" href="<?php echo pathurlencode($zenCSS); ?>" type="text/css" />
-	<link rel="StyleSheet" href="<?php echo SITEURL ?>style.css" type="text/css" media="screen" />
+	<link rel="StyleSheet" href="<?php echo $_zp_themeroot; ?>/css/style.css" type="text/css" media="screen" />
 	<script type="text/javascript">
 		// <!-- <![CDATA[
 		$(document).ready(function(){
